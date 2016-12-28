51a2b08f2fcd126f4bb1cd9cc4f8044c83e45715
linhanqiu <linhanqiu1123@163.com>
2016-12-28 15:26:50 +0800

    修改ngnix.config

--- app/config.py
+++ app/config.py
@@ -5,14 +5,14 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 class BaseConfig:
     SECRET_KEY = '693bda65112eb4b1eab2bfe3fa8e672ad220fa7c'
     UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
-    MAIL_SUBJECT_PREFIX = '[HOTFACE]'
+    MAIL_SUBJECT_PREFIX = '[hotface]'
     MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 25
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = 'linhanqiu1123@163.com'
+    MAIL_DEFAULT_SENDER = 'linhanqiu@163.com'
 
     @staticmethod
     def init_app(app):
@@ -21,7 +21,7 @@ class BaseConfig:
 class DevConfig(BaseConfig):
     MONGO_HOST = '127.0.0.1'
     MONGO_PORT = 27017
-    MONGO_DBNAME = 'admin'
+    MONGO_DBNAME = 'hotface'
     CELERY_IMPORTS = (
         "app.tasks.mail",
     )
